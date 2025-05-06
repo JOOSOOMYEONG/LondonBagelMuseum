3dfa28616c2fe54e210bc09ad02e7ad3ca1fcfdc
JOOSOOMYEONG <JOO2JOJ@GMAIL.COM>
2025-05-06 16:41:53 +0900

    =_=

--- script/script.js
+++ script/script.js
@@ -1,15 +1,15 @@
-        const tabs = document.querySelectorAll('.tab');
-        const tabContent = document.querySelectorAll('.tab_content');
-        console.log(tabs);
-        console.log(tabContent);
+      //   const tabs = document.querySelectorAll('.tab');
+      //   const tabContent = document.querySelectorAll('.tab_content');
+      //   console.log(tabs);
+      //   console.log(tabContent);
 
-        tabs.forEach(tab => {
-           tab.addEventListener('click', () => {
-                tabs.forEach(t => t.classList.remove('active'));
-                tabContent.forEach(tcontent => tcontent.classList.remove('active'));
-                tab.classList.add('active');
-                document.getElementById('tab_' + tab.dataset.tab).classList.add('active');
-                console.log(tab.dataset);
-                console.log(tab.dataset.tab);
-           });
-        });
+      //   tabs.forEach(tab => {
+      //      tab.addEventListener('click', () => {
+      //           tabs.forEach(t => t.classList.remove('active'));
+      //           tabContent.forEach(tcontent => tcontent.classList.remove('active'));
+      //           tab.classList.add('active');
+      //           document.getElementById('tab_' + tab.dataset.tab).classList.add('active');
+      //           console.log(tab.dataset);
+      //           console.log(tab.dataset.tab);
+      //      });
+      //   });
