9589a49a99282b42ae6fbba3c77dd7fcf4dabc09
JOOSOOMYEONG <JOO2JOJ@GMAIL.COM>
2025-05-20 15:23:24 +0900

    =_=

--- script/script.js
+++ script/script.js
@@ -33,3 +33,26 @@ window.addEventListener("keyup", (e)=> {
 function input() {
     window.open("https://www.google.com/maps/search/%EB%9F%B0%EB%8D%98%EB%B2%A0%EC%9D%B4%EA%B8%80%EB%AE%A4%EC%A7%80%EC%97%84" + inputText.value + "/data=!3m1!4b1?entry=ttu&g_ep=EgoyMDI1MDUwNS4wIKXMDSoASAFQAw%3D%3D");
 }
+
+
+
+
+
+
+// 스크롤페이드인
+
+const content = document.getElementsByClassName("content");
+console.log(content);
+window.addEventListener('scroll',()=>{
+    const winH = window.innerHeight; // 전체 화면 높이 1080
+    console.log(winH);
+    for(let i = 0; i < content.length; i++){
+        const contentTop = content[i].getBoundingClientRect().top; 
+        console.log(contentTop);
+        if(contentTop - winH < 0) {
+            content[i].classList.add('in');
+        }else{
+            content[i].classList.remove('in');
+        }
+    }
+});
